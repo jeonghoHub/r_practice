062a04447dcdd26cf59dac0da9955c9bbf32fb42
jeongho <tjsrsdqq4@gmail.com>
2023-09-26 10:12:16 +0900

    practice

--- practice.R
+++ practice.R
@@ -1,1 +1,9 @@
-print("hi")
+ID <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+SEX <- c("F", "M", "F", "M", "F", "F", "F", "M", "F")
+
+dataframe_ex <- data.frame(ID, SEX)
+
+
+dataframe_ex
+
+str(dataframe_ex)
