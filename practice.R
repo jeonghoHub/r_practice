d1441f2285f4329dd4851d23ad61c5e7ad1e349b
jeongho <tjsrsdqq4@gmail.com>
2023-11-27 14:45:03 +0900

    feat : 위도 경도 데이터로 구글 MAP에 마커 찍기

--- practice.R
+++ practice.R
@@ -1,11 +1,23 @@
 library(readxl)
-library(descr)
+library(ggmap)
 
-forest_example_data <- read_excel("D:/download/forest_example_data.xls")
+xlsdata <- read_excel("D:/download/선별진료소_20231127131449.xls")
 
-colnames(forest_example_data) <- c("name", "city", "gubun", "area",
-                                   "number", "stay", "city_new",
-                                   "code", "codename")
+daejeon_data <- data_raw[data_raw$state == "대전",]
 
-stem(forest_example_data$number, scale = 2)
+ggmap_key <- "구글 map API KEY"
+register_google(ggmap_key)
 
+daejeon_data <- mutate_geocode(data = daejeon_data,
+                               location =addr,
+                               source = "google")
+
+daejeon_data_marker <- data.frame(daejeon_data$lon, daejeon_data$lat)
+
+daejeon_map <- get_googlemap("대전", maptype = "roadmap", zoom = 11,
+                             markers = daejeon_data_marker)
+
+ggmap(daejeon_map) +
+  geom_text(data = daejeon_data,
+            aes(x = lon, y = lat),
+            size = 3, label = daejeon_data$name)
